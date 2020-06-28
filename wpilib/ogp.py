72d84180648e21c552a8f2443c95721cb219e6b8
Itay Ziv <itay220204@gmail.com>
2020-06-29 00:32:55 +0300

    Add comments and fix typo

--- wpilib/ogp.py
+++ wpilib/ogp.py
@@ -2,19 +2,24 @@ from urllib.parse import urljoin
 
 
 def get_tags(context, doctree, config):
+    # Get the URL of the specific page
     page_url = urljoin(config["ogp_site_url"], context["pagename"] + context["file_suffix"])
+    # Get the image from the config
     image_url = config["ogp_image"]
+
+    # Get the first 200 letters from the page
     description = doctree.astext().replace('\n', ' ')
 
     if len(description) > 200:
         description = description[:197] + "..."
 
+    # Make the ogp tags
     tags = """
     <meta property="og:title" content="{title}" />
     <meta property="og:type" content="{type}" />
     <meta property="og:url" content="{url}" />
     <meta property="og:description" content="{desc}" />
-    """.format(title=context["title"], type=config["type"], url=page_url, desc=description)
+    """.format(title=context["title"], type=config["ogp_type"], url=page_url, desc=description)
 
     if image_url:
         tags += '<meta property="og:image" content="{image}" />'.format(image=image_url)
