da7c24d7d661401dbf061dbbc91bdd638d1f5711
Itay Ziv <itay220204@gmail.com>
2020-06-29 00:59:17 +0300

    Remove temporary workaround

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ import setuptools
 
 # This will fail if something happens or if not in a git repository.
 # This is intentional.
-#ret = subprocess.run("git describe --tags --abbrev=0", stdout=subprocess.PIPE,
-                     #stderr=subprocess.PIPE, check=True, shell=True)
-version = "0.0.1"#ret.stdout.decode("utf-8").strip()
+ret = subprocess.run("git describe --tags --abbrev=0", stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE, check=True, shell=True)
+version = ret.stdout.decode("utf-8").strip()
 
 with open("readme.md", 'r') as readme:
     long_description = readme.read()
