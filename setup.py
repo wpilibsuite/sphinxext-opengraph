3f6db943bc2fddbb517cffa5f82f6ec8ebf79a37
Itay Ziv <itay220204@gmail.com>
2020-07-01 04:49:12 +0300

    Address comments

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ ret = subprocess.run("git describe --tags --abbrev=0", stdout=subprocess.PIPE,
                      stderr=subprocess.PIPE, check=True, shell=True)
 version = ret.stdout.decode("utf-8").strip()
 
-with open("readme.md", 'r') as readme:
+with open("readme.md", 'r', encoding="utf-8") as readme:
     long_description = readme.read()
 
 setuptools.setup(
@@ -16,15 +16,29 @@ setuptools.setup(
     author="Itay Ziv",
     author_email="itay220204@gmail.com",
     description="Sphinx Extension to enable OGP support",
-    long_descrption=long_description,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/wpilibsuite/sphinxext-opengraph",
     install_requires=['sphinx>=2.0'],
     packages=['sphinxext'],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-        "Framework :: Sphinx :: Extension",
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Environment :: Web Environment',
+        'Framework :: Sphinx :: Extension',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python',
+        'Topic :: Documentation :: Sphinx',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Text Processing',
+        'Topic :: Utilities',
     ],
     python_requires='>=3.6'
 )
