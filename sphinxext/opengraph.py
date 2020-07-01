1d111de4de63fc515385cd6ef22c4f8bfe76ad7b
Vasista Vovveti <vasistavovveti@gmail.com>
2020-07-01 03:51:07 -0400

    Add support for og:site_name

--- sphinxext/opengraph.py
+++ sphinxext/opengraph.py
@@ -4,10 +4,13 @@ DEFAULT_DESCRIPTION_LENGTH = 200
 
 
 def get_tags(context, doctree, config):
+
     # Get the URL of the specific page
     page_url = urljoin(config["ogp_site_url"], context["pagename"] + context["file_suffix"])
     # Get the image from the config
     image_url = config["ogp_image"]
+    # Get the site name from the config
+    site_name = config["ogp_site_name"]
 
     # Get the first X letters from the page (Configured in config)
     description = doctree.astext().replace('\n', ' ')
@@ -28,6 +31,9 @@ def get_tags(context, doctree, config):
     <meta property="og:description" content="{desc}" />
     """.format(title=context["title"], type=config["ogp_type"], url=page_url, desc=description)
 
+    if site_name:
+        tags += f'<meta property="og:site_name" content="{site_name}" />'
+
     if image_url:
         tags += '<meta property="og:image" content="{image}" />'.format(image=image_url)
 
@@ -43,6 +49,7 @@ def setup(app):
     app.add_config_value("ogp_site_url", None, "html")
     app.add_config_value("ogp_description_length", DEFAULT_DESCRIPTION_LENGTH, "html")
     app.add_config_value("ogp_image", None, "html")
+    app.add_config_value("ogp_site_name", None, "html")
     app.add_config_value("ogp_type", "website", "html")
 
     app.connect('html-page-context', html_page_context)
