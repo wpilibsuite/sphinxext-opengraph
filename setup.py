2f7332d635784b501de6820f146f66531543ef01
Vasista Vovveti <vasistavovveti@gmail.com>
2020-07-10 12:57:49 -0400

    Install Package we are testing (#14)
    * Install Package we are testing
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ import setuptools
 
 # This will fail if something happens or if not in a git repository.
 # This is intentional.
-ret = subprocess.run("git describe --tags --abbrev=0", stdout=subprocess.PIPE,
-                     stderr=subprocess.PIPE, check=True, shell=True)
-version = ret.stdout.decode("utf-8").strip()
+try:
+    ret = subprocess.run("git describe --tags --abbrev=0", stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE, check=True, shell=True)
+    version = ret.stdout.decode("utf-8").strip()
+except:
+    version = "master"
 
 with open("README.md", 'r', encoding="utf-8") as readme:
     long_description = readme.read()
