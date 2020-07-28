f9dc9d4dab15278094438612fad34f1fb0433d97
Itay Ziv <itay220204@gmail.com>
2020-07-28 16:33:10 +0300

    Combine insert_tags into html_page_context

--- sphinxext/opengraph/event_handler.py
+++ sphinxext/opengraph/event_handler.py
@@ -1,7 +1,10 @@
 from typing import Dict, Any
 from sphinx.application import Sphinx
 from docutils import nodes
-from .util import insert_tags
+from urllib.parse import urljoin
+from .visitor import OpenGraphVisitor
+from .html_parser import HTMLTextParser
+from .util import make_tag
 
 
 def html_page_context(
@@ -12,4 +15,54 @@ def html_page_context(
     doctree: nodes.document,
 ) -> None:
     if doctree:
-        insert_tags(context, doctree, app.config)
+        return
+    # Set length of description
+    try:
+        desc_len = int(app.config["ogp_description_length"])
+    except ValueError:
+        desc_len = 200
+
+    # parse out any html from the title
+    html_parser = HTMLTextParser()
+    html_parser.feed(context["title"])
+    html_parser.close()
+
+    # grab the description from the page
+    visitor = OpenGraphVisitor(doctree, desc_len)
+    doctree.walkabout(visitor)
+
+    # title tag
+    context["metatags"] += make_tag("og:title", html_parser.text)
+
+    # type tag
+    context["metatags"] += make_tag("og:type", app.config["ogp_type"])
+
+    # url tag
+    # Get the url to the specific page
+    page_url = urljoin(
+        app.config["ogp_site_url"], context["pagename"] + context["file_suffix"]
+    )
+    context["metatags"] += make_tag("og:url", page_url)
+
+    # site name tag
+    site_name = app.config["ogp_site_name"]
+    if site_name:
+        context["metatags"] += make_tag("og:site_name", site_name)
+
+    # description tag
+    context["metatags"] += make_tag("og:description", visitor.description)
+
+    # image tag
+    # Get the image from the app.config
+    image_url = app.config["ogp_image"]
+    if image_url:
+        context["metatags"] += make_tag("og:image", image_url)
+
+    # image alt text
+    # todo: change readme
+    ogp_image_alt = app.config["ogp_image_alt"]
+    if ogp_image_alt:
+        context["metatags"] += make_tag("og:image:alt", ogp_image_alt)
+
+    # custom tags
+    context["metatags"] += "\n".join(app.config["ogp_custom_meta_tags"])
