04d2054dde459ca07e53346b3209c95146a887c4
Itay Ziv <itay220204@gmail.com>
2020-07-29 01:36:09 +0300

    Make sanitize_title a separate function

--- sphinxext/opengraph/event_handler.py
+++ sphinxext/opengraph/event_handler.py
@@ -2,9 +2,8 @@ from typing import Dict, Any
 from sphinx.application import Sphinx
 from docutils import nodes
 from urllib.parse import urljoin
+import util
 from .visitor import OpenGraphVisitor
-from .html_parser import HTMLTextParser
-from .util import make_tag
 
 
 def html_page_context(
@@ -22,47 +21,43 @@ def html_page_context(
     except ValueError:
         desc_len = 200
 
-    # parse out any html from the title
-    html_parser = HTMLTextParser()
-    html_parser.feed(context["title"])
-    html_parser.close()
-
     # grab the description from the page
     visitor = OpenGraphVisitor(doctree, desc_len)
     doctree.walkabout(visitor)
 
-    # title tag
-    context["metatags"] += make_tag("og:title", html_parser.text)
+    # title tag# parse out any html from the title
+    page_title = util.santitize_title(context["title"])
+    context["metatags"] += util.make_tag("og:title", page_title)
 
     # type tag
-    context["metatags"] += make_tag("og:type", app.config["ogp_type"])
+    context["metatags"] += util.make_tag("og:type", app.config["ogp_type"])
 
     # url tag
     # Get the url to the specific page
     page_url = urljoin(
         app.config["ogp_site_url"], context["pagename"] + context["file_suffix"]
     )
-    context["metatags"] += make_tag("og:url", page_url)
+    context["metatags"] += util.make_tag("og:url", page_url)
 
     # site name tag
     site_name = app.config["ogp_site_name"]
     if site_name:
-        context["metatags"] += make_tag("og:site_name", site_name)
+        context["metatags"] += util.make_tag("og:site_name", site_name)
 
     # description tag
-    context["metatags"] += make_tag("og:description", visitor.description)
+    context["metatags"] += util.make_tag("og:description", visitor.description)
 
     # image tag
     # Get the image from the app.config
     image_url = app.config["ogp_image"]
     if image_url:
-        context["metatags"] += make_tag("og:image", image_url)
+        context["metatags"] += util.make_tag("og:image", image_url)
 
     # image alt text
     # todo: change readme
     ogp_image_alt = app.config["ogp_image_alt"]
     if ogp_image_alt:
-        context["metatags"] += make_tag("og:image:alt", ogp_image_alt)
+        context["metatags"] += util.make_tag("og:image:alt", ogp_image_alt)
 
     # custom tags
     context["metatags"] += "\n".join(app.config["ogp_custom_meta_tags"])
--- sphinxext/opengraph/util.py
+++ sphinxext/opengraph/util.py
@@ -1,2 +1,14 @@
+from .html_parser import HTMLTextParser
+
+
 def make_tag(tag: str, content: str) -> str:
     return f'<meta property="{tag}" content="{content}" />\n  '
+
+
+def sanitize_title(title: str) -> str:
+    # take in a title and return a clean version.
+    html_parser = HTMLTextParser()
+    html_parser.feed(title)
+    html_parser.close()
+
+    return html_parser.text
