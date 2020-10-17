f8fa9409db6e7bfd5d5b54535899737b72bc37f5
Vasista Vovveti <vasistavovveti@gmail.com>
2020-10-17 12:16:18 -0500

    Use first image (after refactor) (#26)
    * Use first image (after refactor)
    
    Co-authored-by: tcmetzger <ticon@gmx.net>
    
    * black
    
    * fix first_image no uri edge case
    
    * Use iana instead of wikipedia for mime types
    
    * black
    
    * Address comments
    
    * black
    
    Co-authored-by: tcmetzger <ticon@gmx.net>

--- sphinxext/opengraph/__init__.py
+++ sphinxext/opengraph/__init__.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict
 from urllib.parse import urljoin
+from pathlib import Path
 
 import docutils.nodes as nodes
 from sphinx.application import Sphinx
@@ -8,6 +9,20 @@ from .descriptionparser import get_description
 from .titleparser import get_title
 
 DEFAULT_DESCRIPTION_LENGTH = 200
+
+# A selection from https://www.iana.org/assignments/media-types/media-types.xhtml#image
+IMAGE_MIME_TYPES = {
+    "gif": "image/gif",
+    "apng": "image/apng",
+    "webp": "image/webp",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "bmp": "image/bmp",
+    "heic": "image/heic",
+    "heif": "image/heif",
+    "tiff": "image/tiff",
+}
 
 
 def make_tag(property: str, content: str) -> str:
@@ -55,19 +70,30 @@ def get_tags(
     tags += make_tag("og:description", description)
 
     # image tag
-    # Get the image from the config
+    # Get basic values from config
     image_url = config["ogp_image"]
+    ogp_use_first_image = config["ogp_use_first_image"]
+    ogp_image_alt = config["ogp_image_alt"]
+
+    if ogp_use_first_image:
+        first_image = doctree.next_node(nodes.image)
+        if (
+            first_image
+            and Path(first_image.get("uri", "")).suffix[1:].lower() in IMAGE_MIME_TYPES
+        ):
+            image_url = first_image["uri"]
+            ogp_image_alt = first_image.get("alt", None)
+
     if image_url:
         tags += make_tag("og:image", image_url)
 
-    # Add image alt text (either provided by config or from site_name)
-    ogp_image_alt = config["ogp_image_alt"]
-    if isinstance(ogp_image_alt, str):
-        tags += make_tag("og:image:alt", ogp_image_alt)
-    elif ogp_image_alt and site_name:
-        tags += make_tag("og:image:alt", site_name)
-    elif ogp_image_alt and title:
-        tags += make_tag("og:image:alt", title)
+        # Add image alt text (either provided by config or from site_name)
+        if isinstance(ogp_image_alt, str):
+            tags += make_tag("og:image:alt", ogp_image_alt)
+        elif ogp_image_alt and site_name:
+            tags += make_tag("og:image:alt", site_name)
+        elif ogp_image_alt and title:
+            tags += make_tag("og:image:alt", title)
 
     # custom tags
     tags += "\n".join(config["ogp_custom_meta_tags"])
@@ -91,6 +117,7 @@ def setup(app: Sphinx) -> Dict[str, Any]:
     app.add_config_value("ogp_description_length", DEFAULT_DESCRIPTION_LENGTH, "html")
     app.add_config_value("ogp_image", None, "html")
     app.add_config_value("ogp_image_alt", True, "html")
+    app.add_config_value("ogp_use_first_image", False, "html")
     app.add_config_value("ogp_type", "website", "html")
     app.add_config_value("ogp_site_name", None, "html")
     app.add_config_value("ogp_custom_meta_tags", [], "html")
---
+++ tests/roots/test-first-image/index.rst
@@ -0,0 +1,2 @@
+.. image:: http://example.org/image2.png
+   :alt: Test image alt text
