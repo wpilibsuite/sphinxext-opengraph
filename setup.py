c353754df319ae81795d305f77b4ff52debc2a9a
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-10-05 16:48:35 -0700

    Drop support for EOL Python 3.6

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setuptools.setup(
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
@@ -53,5 +52,5 @@ setuptools.setup(
         "Topic :: Text Processing",
         "Topic :: Utilities",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
 )
