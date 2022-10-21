be9a41d892c184693c2080a84a4540f7a6f021dd
Vasista Vovveti <vasistavovveti@gmail.com>
2022-10-21 01:39:26 -0500

    Revert "Add option to use og:description as description (#71)" (#73)
    This reverts commit 0dc991c06035887e17ca622cb976387320fe7157.

--- tests/test_options.py
+++ tests/test_options.py
@@ -1,6 +1,7 @@
 import pytest
 from sphinx.application import Sphinx
 import conftest
+import os
 
 
 def get_tag(tags, tag_type):
@@ -23,16 +24,6 @@ def test_simple(og_meta_tags):
         description
         == "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at lorem ornare, fringilla massa nec, venenatis mi. Donec erat sapien, tincidunt nec rhoncus nec, scelerisque id diam. Orci vari..."
     )
-
-
-@pytest.mark.sphinx("html", testroot="meta-name-description")
-def test_meta_name_description(meta_tags):
-    og_description = get_tag_content(meta_tags, "description")
-    description = [tag for tag in meta_tags if tag.get("name") == "description"][0].get(
-        "content", ""
-    )
-
-    assert og_description == description
 
 
 @pytest.mark.sphinx("html", testroot="simple")
