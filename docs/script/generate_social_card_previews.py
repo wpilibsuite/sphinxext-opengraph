eebe9b70996feab895957dc5c2016e464cd6e37f
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2025-04-04 20:29:59 +0100

    Bump Ruff to 0.11.4

--- docs/script/generate_social_card_previews.py
+++ docs/script/generate_social_card_previews.py
@@ -26,7 +26,7 @@ eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
 veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum""".split()
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum""".split()  # NoQA: SIM905
 
 kwargs_fig = {
     'image': PROJECT_ROOT / 'docs/_static/og-logo.png',
